4f1f7b0a7824af4779164292355405f2d8f4adc4
ReshVenkatachalapathy <reshma.venkatachalapathy@outlook.com>
2024-05-22 19:42:22 -0400

    Add files via upload
    Updated the logic for rest, feed and play method.

--- PetStatus.cs
+++ PetStatus.cs
@@ -28,32 +28,39 @@ namespace VirtualPetStatusTracker
         public void Feed()
         {
             Hunger -= 2;
-            if (Hunger < 0)
+            String strHunger = Hunger.ToString();
+            /* if (Hunger < 0)
+             {
+                 Hunger = 1; 
+             } */
+            if (strHunger.Contains("-"))
             {
-                Hunger = 1; // Ensure hunger doesn't go below 0
+                Console.WriteLine($"{Name} has been fed fully. Hunger decreased and the health is increased.");
+            }
+            else
+            {
+                Health += 1;
+                Console.WriteLine($"{Name} has been fed. Hunger decreased to {Hunger} and the health is increased to {Health}.");
+
             }
 
-            Health += 1; // Increase health by 1
-            if (Health > 10)
-            {
-                Health = 10; // Ensure health doesn't exceed 10
-            }
 
-            Console.WriteLine($"{Name} has been fed. Hunger decreased and the health is increased.");
         }
 
         public void Play()
         {
-            Happiness += 2; // Increase happiness by 2
-            if (Happiness > 10)
-            {
-                Happiness = 10; // Ensure happiness doesn't exceed 10
-            }
+            Happiness += 2;
 
-            Hunger += 1; // Increase hunger by 1
+            /* if (Happiness > 10)
+             {
+                 Happiness = 10; 
+             }*/
+
+
+            Hunger += 1;
             if (Hunger > 10)
             {
-                Hunger = 10; // Ensure hunger doesn't exceed 10
+                Hunger = 10;
             }
 
             Console.WriteLine($"{Name} is excited after playing. Happiness increased which slightly increased in hunger.");
@@ -61,16 +68,16 @@ namespace VirtualPetStatusTracker
 
         public void Rest()
         {
-            Health += 2; // Increase health by 2
+            Health += 2;
             if (Health > 10)
             {
-                Health = 10; // Ensure health doesn't exceed 10
+                Health = 10;
             }
 
-            Happiness -= 1; // Decrease happiness by 1
+            Happiness -= 1;
             if (Happiness < 0)
             {
-                Happiness = 1; // Ensure happiness doesn't go below 1
+                Happiness = 1;
             }
 
             Console.WriteLine($"{Name} rested. Health improved but decrease in happiness slightly.");
@@ -81,10 +88,10 @@ namespace VirtualPetStatusTracker
         {
             Console.WriteLine($"Please find the status of {Name} ({Type}) below: ");
             Console.WriteLine($"Hunger: {Hunger} out of 10 \nHappiness: {Happiness} out of 10 \nHealth: {Health} out of 10");
-    
 
 
-    }
+
+        }
     }
 
 
